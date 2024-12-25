0f9634b8d06cc46283bfbbbb7571018972556794
Tuan Tran <trantuan271001@gmail.com>
2024-12-26 00:04:14 +0700

    commit

--- src/pages/user/theme/header/index.js
+++ src/pages/user/theme/header/index.js
@@ -1,13 +1,30 @@
 import { memo } from "react";
 import "./style.scss";
+import { CircleUser } from "lucide-react";
+import { Mail } from "lucide-react";
 const Header = () => {
   return (
-    <div className="header__top">
-      <div className="container">
-        <div className="row">
-          <div className="col-6">Left</div>
-          <div className="col-6 top_right">Right</div>
-        </div>
+    <div className="header">
+      {/* Logo */}
+      <div className="header-logo">
+        <img src="logo.png" alt="" />
+      </div>
+
+      {/* Search bar */}
+      <div className="header-search">
+        <input type="text" placeholder="Bạn muốn mua gì..." />
+      </div>
+
+      {/* Delivery method */}
+      <div className="header-delivery">
+        <img src="delivery.png" alt="" />
+        <span>Chọn Phương Thức Nhận Hàng</span>
+      </div>
+
+      {/* Icons */}
+      <div className="header-icons">
+        <CircleUser />
+        <Mail />
       </div>
     </div>
   );
